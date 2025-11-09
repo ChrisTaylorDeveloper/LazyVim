ee8430aaf921992d894043f25b410afb21a493f2
Chris Taylor <chris@christaylordeveloper.co.uk>
2025-11-09 13:51:28 +0700

    Remove a couple of Lazy default keymaps.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Remove the LazyVim mappings k -> gk, j -> gj
+vim.keymap.del({ "n", "x" }, "k")
+vim.keymap.del({ "n", "x" }, "j")
+
+-- But do not remove the LazyVim Up / Down arrow re-maps.
+-- vim.keymap.del({ "n", "x" }, "<Up>")
+-- vim.keymap.del({ "n", "x" }, "<Down>")
