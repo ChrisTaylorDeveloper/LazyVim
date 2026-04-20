bd1dc77f07a4d0ae9d706866f2a9d64779da44a4
Chris Taylor <chris@christaylordeveloper.co.uk>
2026-04-20 08:07:44 +0200

    Add Splits plugin.

---
+++ lua/plugins/splits.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    enabled = true,
+    dir = vim.env.REPOS_PATH .. "splits.nvim",
+    opts = {
+      files = {
+        "foo.txt",
+        "bar.txt",
+      },
+      name = "chris",
+      delay = 0,
+      icons = {},
+    },
+  },
+}
